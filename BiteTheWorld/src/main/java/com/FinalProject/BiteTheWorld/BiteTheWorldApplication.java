b105f39500c11505c7070013f0777aec3d912a8b
EliGach1 <edgach12@gmail.com>
2025-02-20 18:08:42 -0500

    Added Spring Project
    May or may not work

---
+++ BiteTheWorld/src/main/java/com/FinalProject/BiteTheWorld/BiteTheWorldApplication.java
@@ -0,0 +1,13 @@
+package com.FinalProject.BiteTheWorld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiteTheWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiteTheWorldApplication.class, args);
+	}
+
+}
---
+++ BiteTheWorld/src/test/java/com/FinalProject/BiteTheWorld/BiteTheWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FinalProject.BiteTheWorld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiteTheWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
