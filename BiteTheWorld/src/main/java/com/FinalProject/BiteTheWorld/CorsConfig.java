e524e49714e9c82e752c87490d9d9968e2d07d22
EliGach1 <edgach12@gmail.com>
2025-03-11 17:47:19 -0400

    added config to the gitignore

--- BiteTheWorld/src/main/java/com/FinalProject/BiteTheWorld/CorsConfig.java
+++ BiteTheWorld/src/main/java/com/FinalProject/BiteTheWorld/CorsConfig.java
@@ -7,6 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
+    //Allows for communication between the backend and the frontend
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
