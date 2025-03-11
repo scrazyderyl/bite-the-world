1307ba0a87e3c9f935640ab77bf302a9ee433d0d
KenBarrett <KTB25@pitt.edu>
2025-03-11 12:59:23 -0400

    got country selection to work on front end

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useRef, useEffect } from 'react'
+import { useRef, useEffect, useState } from 'react'
 import mapboxgl from 'mapbox-gl'
 
 import 'mapbox-gl/dist/mapbox-gl.css';
@@ -7,20 +7,41 @@ import './App.css'
 
 function App() {
 
+  const [countryname, setCountryname] = useState(null);
   const mapRef = useRef()
   const mapContainerRef = useRef()
+
+  async function fetchCountry(lng, lat) {
+    try {
+      const response = await fetch(`https://api.mapbox.com/search/geocode/v6/reverse?longitude=${lng}&latitude=${lat}&access_token=${mapboxgl.accessToken}`);
+      const data = await response.json();
+      return data; // Return data for use outside the function
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  }
 
   useEffect(() => {
     mapboxgl.accessToken = 'pk.eyJ1Ijoia2VuYmFycmV0dCIsImEiOiJjbTdwNnNjZ3EwazkzMmtwdTUyd245OWZzIn0.kpXLmwdN386GWurljXEuaw'
     mapRef.current = new mapboxgl.Map({
       container: mapContainerRef.current,
-      style: 'mapbox://styles/mapbox/streets-v11', 
-      center: [-74.5, 40], 
-      zoom: 1, 
+      style: 'mapbox://styles/kenbarrett/cm84ln2qt005l01qid128djhw',
+      center: [5, 10],
+      zoom: 1.5
+    });
+
+    mapRef.current.on('click', async (event) => {
+      const { lng, lat } = event.lngLat; // Get clicked coordinates
+      console.log(`Clicked at Longitude: ${lng}, Latitude: ${lat}`);
+      let countrydata = await fetchCountry(lng, lat);
+      setCountryname(countrydata.features[0].properties.context.country.name);
+      console.log(countryname);
+      
     });
 
     return () => {
-      mapRef.current.remove()
+      mapRef.current.remove();
+      mapRef.current.off('click');
     }
   }, [])
 
@@ -35,7 +56,7 @@ function App() {
             <a href="/map" style={styles.link}>User</a>
           </li>
           <li style={styles.li}>
-            <a href="/about" style={styles.link}>Recipes</a>
+            { countryname ? <a href="/about" style={styles.link}>Recipes for {countryname}</a> : null}
           </li>
         </ul>
       </nav>
