ec6b34668282308dfa9eef5bb128a0c6ba86a09e
Gabi <124410388+Gabifern@users.noreply.github.com>
2025-03-11 10:49:14 -0400

    Include Nav Bar on homepage with 2D map

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,6 +14,9 @@ function App() {
     mapboxgl.accessToken = 'pk.eyJ1Ijoia2VuYmFycmV0dCIsImEiOiJjbTdwNnNjZ3EwazkzMmtwdTUyd245OWZzIn0.kpXLmwdN386GWurljXEuaw'
     mapRef.current = new mapboxgl.Map({
       container: mapContainerRef.current,
+      style: 'mapbox://styles/mapbox/streets-v11', 
+      center: [-74.5, 40], 
+      zoom: 9, 
     });
 
     return () => {
@@ -23,9 +26,55 @@ function App() {
 
   return (
     <>
-      <div id='map-container' ref={mapContainerRef}/>
+     <nav style={styles.nav}>
+        <ul style={styles.ul}>
+          <li style={styles.li}>
+            <a href="/" style={styles.link}>Map</a>
+          </li>
+          <li style={styles.li}>
+            <a href="/map" style={styles.link}>User</a>
+          </li>
+          <li style={styles.li}>
+            <a href="/about" style={styles.link}>Recipes</a>
+          </li>
+        </ul>
+      </nav>
+
+      <div id="map-container" ref={mapContainerRef} style={styles.mapContainer} />
     </>
   )
 }
+
+const styles = {
+  nav: {
+    backgroundColor: '#333',
+    padding: '10px',
+    position: 'fixed', 
+    top: 0,
+    left: 0,
+    right: 0,
+    zIndex: 1000, 
+  },
+  ul: {
+    listStyle: 'none',
+    display: 'flex',
+    justifyContent: 'center',
+    margin: 0,
+    padding: 0,
+  },
+  li: {
+    margin: '0 15px',
+  },
+  link: {
+    color: '#fff',
+    textDecoration: 'none',
+    fontSize: '18px',
+  },
+  mapContainer: {
+    height: '100vh', 
+    width: '100%',
+    marginTop: '60px', 
+  },
+};
 
 export default App
