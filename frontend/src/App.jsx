c73dd71991bf274a4a36e662bb94da71adfc95d3
Gabi <124410388+Gabifern@users.noreply.github.com>
2025-03-11 10:53:08 -0400

    Update Zoom - pushed out

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
       container: mapContainerRef.current,
       style: 'mapbox://styles/mapbox/streets-v11', 
       center: [-74.5, 40], 
-      zoom: 9, 
+      zoom: 1, 
     });
 
     return () => {
